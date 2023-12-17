c0bfae1475d73509bec221b1a8fe900597cdd02e
haiwenw <haiwenwu@foxmail.com>
2023-12-17 13:37:09 +0800

    option pricing files

---
+++ optionPricing/cdf.h
@@ -0,0 +1,10 @@
+#ifndef CDF_H
+#define CDF_H
+
+#include<cmath>
+
+const double pi = 4.0*atan(1.0);
+
+double CDF(double);
+
+#endif
--- optionPricing/main.cpp
+++ optionPricing/main.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<cmath>
 #include<fstream>
+#include"cdf.h"
 
 using namespace std;
 
-const double pi = 4.0*atan(1.0);
 
-double CDF(double);
 
 int main()
 {
@@ -29,31 +28,4 @@ int main()
     out.close();
 
     return 0;
-}
-
-double CDF(double X)
-{
-    const double a1 = 0.319381530, a2 = -0.356563782, a3 = 1.781477937;
-    const double a4 = -1.821255978, a5 = 1.330274429;
-
-    double x = 0, k=0;
-    double N, CDF, n;
-
-    x = fabs(X);
-
-    k = 1/(1+0.2316419*x);
-
-    n = (1/sqrt(2*pi)) * exp(-0.5*x*x);
-
-    N = 1-n*(a1*k + a2*k*k +a3*pow(k,3) + a4*pow(k,4) + a5*pow(k,5));
-
-    if (X>=0)
-    {
-        CDF = N;
-    }
-    else
-    {
-        CDF = 1-N;
-    }
-    return CDF;
 }
